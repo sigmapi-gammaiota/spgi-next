1febb2a8c2c3dc5b227b07153d916427b9774a1e
KyleWynnM <mcfatterkyle@gmail.com>
2022-04-20 13:26:10 -0400

    updated home page content margins

--- pages/public/index.tsx
+++ pages/public/index.tsx
@@ -15,8 +15,9 @@ export const getStaticProps: GetStaticProps = async () => {
 
 export default function Page() {
   return <div>
-      <h2 className="text-2xl text-purple-900 mt-10">Welcome to Sigma Pi</h2>
-      <p>
+      <div className="mt-10 ml-5 mr-5">
+        <h2 className="text-2xl text-purple-900">Welcome to Sigma Pi</h2>
+        <p className="text-justify">
           Gamma Iota chapter began in 1963 as Psi Theta Nu Fraternity, 
           a local fraternity comprised of like-minded individuals at 
           Worcester Polytechnic Institute. As a local fraternity, 
@@ -26,11 +27,14 @@ export default function Page() {
           of Sigma Pi Fraternity. After another year, on May 1st, 1965, 
           Psi Theta Nu received its charter from Sigma Pi Fraternity.
         </p>
+      </div>
+      <div className="mt-10 ml-5 mr-5">
         <h2 className="text-2xl text-purple-900 mt-10">In Memory of Jyn Godin</h2>
-        <p>
+        <p className="text-justify">
         Born on December 23rd, 2000, Jyn brought twenty years of joy to all those around them. They joined our chapter in the fall of 2019 as a member of Sigma class. Jyn was part of a group where they could freely learn and share their passions with others. Jyn embodied the culture and character of Sigma Pi; the absence of their enthusiasm and care is felt every day.
         Jyn is not just our friend, they are our brother, and they always will be.
         </p>
         <p>more content (update 2019 Greek Week)</p>
+        </div>
   </div>;
 }
