26294309536465ada7a106afb7cc79d886d7f395
KyleWynnM <mcfatterkyle@gmail.com>
2022-05-10 16:01:12 -0400

    added amazing day picture

--- pages/public/philanthropy.tsx
+++ pages/public/philanthropy.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from "next";
 import { getDefaultPublicProps } from "../../lib/NextProps";
-
+import Image from 'next/image'
 export const getStaticProps: GetStaticProps = async () => {
   let p = getDefaultPublicProps();
   p.title = "Philanthropy";
@@ -13,13 +13,13 @@ export default function Page() {
   return (
 <div className="py-16 px-8 md:px-32 bg-base1 flex flex-wrap gap-6">
       <div className="w-full h-28 flex flex-col justify-center items-center gap-6 text-white bg-purp0 text-white">
-        <h1>Philanthropy</h1>
+        <h1 className="text-3xl">Philanthropy</h1>
       </div>
 <div className="">
 
       <div className="max-w-6xl mx-auto grid grid-cols-1">
-        <div className="py-16 px-32 bg-base0"> 
-        <h2 className="mt-5">Overview</h2>
+        <div className="py-16 px-32 bg-base0">
+        <h2 className="mt-5 text-2xl">Overview</h2>
 
         <p>Our chapter's activities fall into five main categories:
 
@@ -33,9 +33,9 @@ export default function Page() {
         </p>
         </div>
 <div className=" py-16 px-32 bg-base1">
-        <h2 className="mt-5">Philanthropy & Community Service</h2>
+        <h2 className="mt-5 text-2xl">Philanthropy & Community Service</h2>
 
-        <p>Our brotherhood considers community service with high importance, and we strive to give back to the Worcester and WPI community. 
+        <p>Our brotherhood considers community service with high importance, and we strive to give back to the Worcester and WPI community.
           Throughout the 2016/2017 academic year our brothers contributed over 1000 hours across 47 unique service events. Whether as mentors for FIRST Robotics teams or farmhands for local farmers,
           our chapter has made lasting contributions to the area.</p>
         <div className="m-1 p-0"></div>
@@ -46,21 +46,26 @@ export default function Page() {
 
 </div>
 <div className=" py-16 px-32 bg-base2">
-        <h2 className="mt-5">Freshman Move-In</h2>
+        <h2 className="mt-5 text-2xl">Freshman Move-In</h2>
 
         <p>At the beginning of each academic year, our chapter joins with other Greek chapters on campus, and helps incoming freshmen move into their dormitories. We welcome each student with cheers and help carry their belongings to their rooms. This effort not only allows incoming freshmen to move in smoothly and rapidly, but also serves as a welcoming gesture toward the new class from the fraternities and sororities on campus.</p>
 
 
-        <h2 className="mt-5">Relay for Life</h2>
+        <h2 className="mt-5 text-2xl">Relay for Life</h2>
 
         <p>Each spring our chapter participates in the WPI Relay for Life put on by the American Cancer Society. Leading up to the event our brotherhood raises money for cancer research through fundraising and personal contributions. During the event our brothers walk in support of those who have battled cancer.</p>
 
-        <h2 className="mt-5">ACE Project</h2>
+        <h2 className="mt-5 text-2xl">ACE Project</h2>
 
         <p>An Altruistic Campus Experience (ACE) is an annual project that each chapter of Sigma Pi completes in benefit of their host institution. For our chapter, this project serves to foster the unique and supportive community that WPI has to offer. Past ACE projects have included supporting the Amazing Day Foundation and promoting mental health.</p>
 
-        <h2 className="mt-5">Amazing Day</h2>
-
+        <h2 className="mt-5 text-2xl">Amazing Day</h2>
+<Image
+src="/Amazing-day-2022.jpg"
+height="3024"
+width="4032"
+objectFit="contain"
+/>
         <p>
         The Amazing Day Foundation exists in support of suicide awareness and prevention. This topic is of extreme importance particularly on college campuses, where an estimated 1 in 12 students have strongly considered suicide.
         </p><div className="m-1 p-0"></div>
